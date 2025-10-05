1682b8159aeca96e7a7f6e67f79e1420dc9e8a97
Dmytro Tymchuk <d.tym@ukr.net>
2025-10-04 19:10:40 -0500

    updates

--- components/TagsMenu/TagsMenu.tsx
+++ components/TagsMenu/TagsMenu.tsx
@@ -1,38 +1,49 @@
-import { NoteTag } from '@/types/note';
-import css from './TagsMenu.module.css'
-import Link from 'next/link';
+'use client'
+
+import { useEffect, useRef, useState } from "react";
+import Link from "next/link";
+import { NoteTag } from "@/types/note";
+import css from "./TagsMenu.module.css";
 
 const TagsMenu = () => {
-    const tags: NoteTag[] = ['Todo', 'Work', 'Personal', 'Meeting', 'Shopping'];
+  const [open, setOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const tags: NoteTag[] = ["Todo", "Work", "Personal", "Meeting", "Shopping"];
 
-    return (
-        <div className={css.menuContainer}>
-  <button className={css.menuButton}>
-    Notes ▾
-  </button>
-    <ul className={css.menuList}>
-                {/* список тегів */}
-                <li  className={css.menuItem}>
-        <Link 
-              href={`/notes/filter/all`} 
-              className={css.menuLink}
-            >
-             All Notes
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  return (
+    <div ref={menuRef} className={css.menuContainer}>
+      <button className={css.menuButton} onClick={() => setOpen((prev) => !prev)}>
+        Notes ▾
+      </button>
+
+      {open && (
+        <ul className={css.menuList}>
+          <li className={css.menuItem}>
+            <Link href="/notes/filter/all" className={css.menuLink}>
+              All Notes
             </Link>
-      </li>
+          </li>
+          {tags.map((tag, index) => (
+            <li key={index} className={css.menuItem}>
+              <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
+                {tag}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
-    {tags.map((tag, index) => <li key={index} className={css.menuItem}>
-        <Link 
-              href={`/notes/filter/${tag}`} 
-              className={css.menuLink}
-            >
-              {tag}
-            </Link>
-      </li>)}
-      
-    </ul>
-</div>
-    )
-}
-
-export default TagsMenu
+export default TagsMenu;
